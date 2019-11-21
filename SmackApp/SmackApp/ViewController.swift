658f449912da3add30dc7d44d45c2072b0b48856
Thomas Pigg <thomaspigg@Thomass-MBP.home>
2019-11-21 13:49:47 +0000

    first setup

---
+++ SmackApp/SmackApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackApp
+//
+//  Created by Thomas Pigg on 21/11/2019.
+//  Copyright © 2019 Thomas Pigg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
